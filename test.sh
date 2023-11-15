8ef32269bdd57c207e4aa7f444789aceb16abb75
Alexandre Alapetite <alexandre.alapetite@alexandra.dk>
2023-11-15 15:41:04 +0100

    Reject password inputs looking like raw bcrypt (#9)
    * Reject password inputs looking like raw bcrypt
    If a password arrives in the authorization header as a string looking like bcrypt (according to standard Modular Crypt Format) instead of normal-looking plain-text, then reject the authentication.
    This is to avoid being able to pass the crypted version of the password, in case it was leaked.
    
    * Use explicit base64 in test examples
    https://github.com/alexandrainst/node-red-http-basic-auth/pull/9#discussion_r1387642267

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Accept password saved as bcrypt, also a second attempt (cached)
+
 test=$(
-	cat <<'EOF' | node ./index.js http-basic-auth --realm='"node-red"' --username='"test"' --password='"$2y$10$5TSZDldoJ7MxDZdtK/SG2O3cwORqLDhHabYlKX9OsM.W/Z/oLwKW6"'
-{"req":{"headers":{"authorization":"Basic dGVzdDp0ZXN0"}}}
-{"req":{"headers":{"authorization":"Basic dGVzdDp0ZXN0"}}}
+	cat <<EOF | node ./index.js http-basic-auth --realm='"node-red"' --username='"test"' --password='"$2y$10$5TSZDldoJ7MxDZdtK/SG2O3cwORqLDhHabYlKX9OsM.W/Z/oLwKW6"'
+{"req":{"headers":{"authorization":"Basic $(printf 'test:test' | base64 -w 0)"}}}
+{"req":{"headers":{"authorization":"Basic $(printf 'test:test' | base64 -w 0)"}}}
 EOF
 )
 
@@ -12,9 +14,11 @@ if [ "$test" = "" ]; then
 	exit 1
 fi
 
+# Accept password saved as plain-text
+
 test=$(
-	cat <<'EOF' | node ./index.js http-basic-auth --realm='"node-red"' --username='"test"' --password='"test"'
-{"req":{"headers":{"authorization":"Basic dGVzdDp0ZXN0"}}}
+	cat <<EOF | node ./index.js http-basic-auth --realm='"node-red"' --username='"test"' --password='"test"'
+{"req":{"headers":{"authorization":"Basic $(printf 'test:test' | base64 -w 0)"}}}
 EOF
 )
 
@@ -23,9 +27,11 @@ if [ "$test" = "" ]; then
 	exit 1
 fi
 
+# Reject wrong user
+
 test=$(
-	cat <<'EOF' | node ./index.js http-basic-auth --realm='"node-red"' --username='"wrong"' --password='"$2y$10$5TSZDldoJ7MxDZdtK/SG2O3cwORqLDhHabYlKX9OsM.W/Z/oLwKW6"'
-{"req":{"headers":{"authorization":"Basic dGVzdDp0ZXN0"}}}
+	cat <<EOF | node ./index.js http-basic-auth --realm='"node-red"' --username='"wrong"' --password='"test"'
+{"req":{"headers":{"authorization":"Basic $(printf 'test:test' | base64 -w 0)"}}}
 EOF
 )
 
@@ -34,13 +40,28 @@ if [ "$test" != "" ]; then
 	exit 1
 fi
 
+# Reject wrong password
+
 test=$(
-	cat <<'EOF' | node ./index.js http-basic-auth --realm='"node-red"' --username='"test"' --password='"wrong"'
-{"req":{"headers":{"authorization":"Basic dGVzdDp0ZXN0"}}}
+	cat <<EOF | node ./index.js http-basic-auth --realm='"node-red"' --username='"test"' --password='"wrong"'
+{"req":{"headers":{"authorization":"Basic $(printf 'test:test' | base64 -w 0)"}}}
 EOF
 )
 
 if [ "$test" != "" ]; then
 	echo 'ERROR 4'
+	exit 1
+fi
+
+# Do not accept bcrypt passwords as input (only plain-text passwords)
+
+test=$(
+	cat <<EOF | node ./index.js http-basic-auth --realm='"node-red"' --username='"test"' --password='"$2y$10$5TSZDldoJ7MxDZdtK/SG2O3cwORqLDhHabYlKX9OsM.W/Z/oLwKW6"'
+{"req":{"headers":{"authorization":"Basic $(printf 'test:$2y$10$5TSZDldoJ7MxDZdtK/SG2O3cwORqLDhHabYlKX9OsM.W/Z/oLwKW6' | base64 -w 0)"}}}
+EOF
+)
+
+if [ "$test" != "" ]; then
+	echo 'ERROR 5'
 	exit 1
 fi
